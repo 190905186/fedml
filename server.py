a892e82ae03332c02ffe9d54158b35aa329ee820
sivasai <57454345+190905186@users.noreply.github.com>
2023-04-18 16:22:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ import flwr as fl
 
 # Start Flower server
 fl.server.start_server(
-        server_address="172.16.58.37:8080",
+        server_address="localhost:8080",
     config=fl.server.ServerConfig(num_rounds=3),
 )                               
