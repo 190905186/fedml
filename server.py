7e7da031cd433d9bce42d514078d0f1f9e7668dd
sivasai <57454345+190905186@users.noreply.github.com>
2023-04-20 15:06:41 +0530

    Update server.py
    added ipaddr log

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 
 import flwr as fl
+import socket
 
+ipaddr=socket.gethostbyname(socket.gethostname())
+print("the server ipaddr is ", ipaddr)
 
 # Start Flower server
 fl.server.start_server(
-        server_address="localhost:8080",
+        server_address=f"{ipaddr}:8080",
     config=fl.server.ServerConfig(num_rounds=3),
 )                               
